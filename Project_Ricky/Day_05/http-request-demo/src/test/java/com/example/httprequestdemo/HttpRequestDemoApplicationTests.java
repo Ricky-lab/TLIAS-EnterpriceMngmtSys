f76a585f9f09bedee32658cb6872d50e5c185c4c
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2023-09-03 21:25:12 -0400

    Add files via upload

---
+++ Project_Ricky/Day_05/http-request-demo/src/test/java/com/example/httprequestdemo/HttpRequestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httprequestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRequestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
