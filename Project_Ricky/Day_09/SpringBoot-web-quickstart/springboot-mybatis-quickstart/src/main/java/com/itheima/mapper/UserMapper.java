8c91b9faca50856b98954be2a0ccfc7c98986b7d
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2023-09-03 21:25:37 -0400

    Add files via upload

---
+++ Project_Ricky/Day_09/SpringBoot-web-quickstart/springboot-mybatis-quickstart/src/main/java/com/itheima/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.itheima.mapper;
+
+import com.itheima.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    @Select("select * from user")
+    public List<User> list();
+}
